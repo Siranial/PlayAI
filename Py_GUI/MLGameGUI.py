105d78fdd896a13ab81994158a967daf757d80d7
Jonathan Adams <jonadams@udel.edu>
2022-11-01 16:48:37 -0400

    reorganized GUI code

--- Py_GUI/MLGameGUI.py
+++ Py_GUI/MLGameGUI.py
@@ -1,6 +1,12 @@
+import os
 import PySimpleGUI as sg
+import wexpect as wx
 
-layout = [[sg.Text("Select a game to run")], [sg.Button("Pong"), sg.Button("SpaceInvaders")], [sg.Button("Exit")]]
+# The layout of the GUI
+layout = [
+        [sg.Text("Select a game to run")], 
+        [sg.Button("Pong"), sg.Button("SpaceInvaders")],
+        [sg.Button("Exit")]]
 
 # Create the window
 window = sg.Window("PlayAI", layout, margins=(200, 200))
@@ -8,8 +14,11 @@ window = sg.Window("PlayAI", layout, margins=(200, 200))
 # Create an event loop
 while True:
     event, values = window.read()
+    #Launch Pong.py if user presses Pong button
+    if event == "Pong":
+        Pong_Window = wx.spawn("..\Pong_Game\Pong.py")
     # End program if user closes window or
-    # presses the OK button
+    # presses the Exit button
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
 
