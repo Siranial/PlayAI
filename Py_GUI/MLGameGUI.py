b882d4efdb15f9123b7c395e3a6a59f9105c1f9a
Jonathan Adams <jonadams@udel.edu>
2022-11-01 16:18:20 -0400

    Implemented simple GUI for PlayAI.py program

--- Py_GUI/MLGameGUI.py
+++ Py_GUI/MLGameGUI.py
@@ -1,3 +1,16 @@
 import PySimpleGUI as sg
 
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+layout = [[sg.Text("Select a game to run")], [sg.Button("Pong"), sg.Button("SpaceInvaders")], [sg.Button("Exit")]]
+
+# Create the window
+window = sg.Window("PlayAI", layout, margins=(200, 200))
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
