d9700a3cc6086cd3f3f193cde4c2dfed52c72d34
Jonathan Adams <jonadams@udel.edu>
2022-11-10 16:05:03 -0500

    added get process task

---
+++ PlayAI_GUI/getProcess.py
@@ -0,0 +1,15 @@
+#import tkinter as tk
+#from tkinter import ttk
+#import tkinter.font as font
+#import winsound, pickle
+import psutil as ps
+
+#Wait for user to input game name
+print("Please enter name of game")
+p_name = input()
+
+#Find process with game name
+for p in ps.process_iter():
+    if p_name in p.name():
+        print(p.name())
+        break
