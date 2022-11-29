d297441e1f2e62bf3a4d4139bf343cfff00e6187
Favistania <noelhang@udel.edu>
2022-11-29 16:32:23 -0500

    Gui menu buttons
    they don't do anything yet

--- PlayAI_GUI/MLGameGUI.py
+++ PlayAI_GUI/MLGameGUI.py
@@ -2,16 +2,25 @@ import os, subprocess
 import PySimpleGUI as sg
 import pexpect as px
 
+# The menu of the GUI
+menu_def = ['&File', ['&New File', '&Open...','Open &Module','---', '!&Recent Files','C&lose']],['&Save',['&Save File', 'Save &As','Save &Copy'  ]],['&Edit', ['&Cut', '&Copy', '&Paste']]
 
 # The layout of the GUI
 layout = [
+        [sg.Menu(menu_def)],
         [sg.Text("Select a game to run")], 
         [sg.Button("Pong"), sg.Button("SpaceInvaders")],
         [sg.Button("Exit")]]
+
+#Gui Theme
 sg.theme("Dark Grey 15")
+
+#Gui Theme Preview
+#sg.theme_previewer()
+
 # Create the window
 window = sg.Window("PlayAI", layout, margins=(200, 200))
-sg.theme_previewer()
+
 # Create an event loop
 while True:
     event, values = window.read()
