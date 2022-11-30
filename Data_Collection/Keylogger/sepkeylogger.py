48215e8676a9f841626d3b3ed686c1de0c472e62
Jonathan Adams <jonadams@udel.edu>
2022-11-30 11:20:42 -0500

    Added frame capture and framework for integrated keylogging

--- Data_Collection/Keylogger/sepkeylogger.py
+++ Data_Collection/Keylogger/sepkeylogger.py
@@ -1,18 +1,32 @@
 import os
+import cv2
 import numpy as np
-import pynput
+import pyautogui
+import pygetwindow as gw
+import sys
 from pynput.keyboard import Key, Listener
 
+#Keylogging
 filepath = ''
-file_name = '/keylog0.txt'
+file_name = ''
 keys = []
 
+#Video capture
+window_name = ''
+frameNum = 0
+
 #Initialize keylog file path
-def init_keylogger():
+def init_log_var():
+    #init globals
+    global filepath
+    global file_name
+    global window_name
+
     #Poll user where to store keylogs
     print('Please input file location for storing keylogs')
     print('Example: C:/Users/USERNAME/Desktop')
     filepath = input()
+
     #Ensure filepath exists
     while not os.path.exists(filepath):
             print('Folder not found, please enter existing folder location')
@@ -20,9 +34,13 @@ def init_keylogger():
 
     #Check what to call keylog file and set file name
     i = 0
-    while os.path.exists(filepath + file_name):
+    file_name = '/log0'
+    while os.path.exists(filepath + file_name + '.txt'):
         i+=1
-        file_name = '/keylog' + str(i) + '.txt'
+        file_name = '/log' + str(i)
+    
+    print('Please input the name of the window to be recorded')
+    window_name = input()
 
 #Detect when a key is pressed
 def on_press(key):
@@ -31,10 +49,15 @@ def on_press(key):
 
 #Write keys and framestamps to file
 def write_file(keys):
-    with open(filepath + file_name, 'w') as f:
+    #init globals
+    global filepath
+    global file_name
+    global frameNum
+
+    with open(filepath + file_name + '.txt', 'w') as f:
         for key in keys:
             # removing '' and writing to file
-            f.write(str(key).replace("'", ""))
+            f.write(str(key).replace("'", "") + ',' + str(frameNum))
             # add new line
             f.write('\n')
 
@@ -45,8 +68,44 @@ def on_release(key):
         # Stop listener
         return False
   
-  
-with Listener(on_press = on_press,
-              on_release = on_release) as listener:
-                     
+
+#Initialize variables
+init_log_var()
+
+cap = cv2.VideoCapture(0)
+# define the codec
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+# frames per second
+fps = 12.0
+# search for the window, getting the first matched window with the title
+w = gw.getWindowsWithTitle(window_name)[0]
+# activate the window
+w.activate()
+# create the video write object
+out = cv2.VideoWriter(filepath + file_name + '.avi', fourcc, fps, tuple(w.size))
+
+#Start recording keys and video
+with Listener(on_press = on_press,on_release = on_release) as listener:
     listener.join()
+
+while True:
+    # make a screenshot
+    img = pyautogui.screenshot(region=(w.left, w.top, w.width, w.height))
+    # convert these pixels to a proper numpy array to work with OpenCV
+    frame = np.array(img)
+    # convert colors from BGR to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # write the frame
+    out.write(frame)
+
+    #Read stdin and write all keys from buffer into file with frame number
+
+    frameNum+=1
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+# make sure everything is closed when exited
+cap.release()
+out.release()
+cv2.destroyAllWindows()
