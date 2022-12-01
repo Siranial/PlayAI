dc03854ed40d3eb7b44965e5adb18462babacdb1
Jonathan Adams <jonadams@udel.edu>
2022-12-01 16:42:16 -0500

    implement video recording into video file

--- Python_Playground/rtvision/main.py
+++ Python_Playground/rtvision/main.py
@@ -11,9 +11,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 # initialize the WindowCapture class
-wincap = WindowCapture('Pygame Pong')
+print('Please input the name of the window to be recorded')
+window_name = input()
+wincap = WindowCapture(window_name)
 # initialize the Vision class
-target = Vision('pong_ball.png')
+#target = Vision('pong_ball.png')
 
 '''
 # https://www.crazygames.com/game/guns-and-bottle
@@ -21,19 +23,33 @@ wincap = WindowCapture()
 vision_gunsnbottle = Vision('gunsnbottle.jpg')
 '''
 
+#Define video codec
+fourcc = cv.VideoWriter_fourcc(*"XVID")
+# define frames per second
+fps = 12.0
+# create the video write object
+out = cv.VideoWriter('video.avi', fourcc, fps, tuple((wincap.w, wincap.h)))
+
 loop_time = time()
 while(True):
 
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
+    # convert screenshot to np array for opencv
+    frame = np.array(screenshot)
+    # convert colors from BGR to RGB
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    # write the frame
+    out.write(frame)
+
     # display the processed image
-    points = target.find(screenshot, 0.5, 'rectangles')
+    #points = target.find(screenshot, 0.5, 'rectangles')
     #points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
 
     # debug the loop rate
-    print('FPS {}'.format(1 / (time() - loop_time)))
-    loop_time = time()
+    #print('FPS {}'.format(1 / (time() - loop_time)))
+    #loop_time = time()
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
