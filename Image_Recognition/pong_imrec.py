abb45b6bb26720082401e779b3f27c6833eb2269
Jonathan Adams <jonadams@udel.edu>
2022-12-05 18:27:10 -0500

    implemented bar following ball

--- Image_Recognition/pong_imrec.py
+++ Image_Recognition/pong_imrec.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 import os
+import time
+import win32con, win32api
 from windowcapture import WindowCapture
 from vision import Vision
 
@@ -21,24 +23,43 @@ while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
     # convert screenshot to np array for opencv
-    frame = np.array(screenshot)
+    #frame = np.array(screenshot)
     # convert colors from BGR to RGB
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    #frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
     # find the coordinates where the targets lie
-    pointsBall, frameRect = targetBall.find(frame, 0.4, 200, 'points')
-    pointsBar, frameRect = targetBar.find(frameRect, 0.4, 1, 'rectangles')
+    pointsBall, frameRect = targetBall.find(screenshot, 0.4, 150, 'points')
+    pointsBar, frameRect = targetBar.find(frameRect, 0.39, 30, 'rectangle')
 
-    #COMBINE POINTSBALL AND BAR HERE TO MAKE THE IMAGE AND PRINT IT
-    #need to edit vision.py to return the points only
+    #Get the rightmost recognized bar
+    if len(pointsBar) > 0:
+        pointRightBar = (0,0)
+        for point in pointsBar:
+            if point[0] > pointRightBar[0]:
+                pointRightBar = point
+            
 
+    #Calculate difference between ball and right paddle 
+    #coordinates and send bar towards ball y coordinate
+    if len(pointsBall):
+        if pointRightBar[1] > pointsBall[0][1]:
+            moveBar = win32con.VK_UP
+        else:
+            moveBar = win32con.VK_DOWN
+        
+        #Move bar
+        win32api.PostMessage(wincap.hwnd, win32con.WM_KEYDOWN, moveBar, 0)
+        time.sleep(0.5)
+        win32api.PostMessage(wincap.hwnd, win32con.WM_KEYUP, moveBar, 0)
+        time.sleep(0.5)
+        
 
     #Show the points where ball is recognized
     cv.imshow('Ball matches', frameRect)
 
     # polls for q press to quit capture
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+    #if cv.waitKey(1) == ord('q'):
+    #    cv.destroyAllWindows()
+    #    break
 
 
