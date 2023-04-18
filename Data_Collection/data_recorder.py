2d88167a89617c9ceed1eb5afddd4bf108d0c363
Jonathan Adams <jonadams@udel.edu>
2023-04-18 14:45:51 -0400

    changed class name of key capture

--- Data_Collection/data_recorder.py
+++ Data_Collection/data_recorder.py
@@ -1,7 +1,8 @@
 from framecapture import FrameCapture
-from keycapture import KeyCapture
+from keycapture import KeyListener
 import os
 import pygetwindow as gw
+import time
 
 class DataRecorder():
     def __init__(self, program_name, session_name):
@@ -32,7 +33,9 @@ class DataRecorder():
 
         # Get keys of interest from the user
         keys = input('Which keys should the logger listen to? Enter up to 10 keys each separated by a comma. For example: w,a,s,d\n')
-        self.keycap = KeyCapture(self.window, keys_folder_path, keys)
+        #self.keycap = KeyCapture(self.window, keys_folder_path, keys)
+        self.keycap = KeyListener(keys_folder_path, keys)
+        self.keycap.start_listen()
 
         # Activate recording window
         self.window.activate()
@@ -42,7 +45,15 @@ class DataRecorder():
         self.keycap.write_keys(frame_number)
 
 
+fps = 10.0
+
 recorder = DataRecorder("Discord", "Test")
 
+last_time = time()
+time.sleep(1.0 / fps)
+current_time = time()
+
 while True:
-    recorder.capture()
+    # Make sure that the recorder does not operate faster than the fps
+    if (current_time - last_time) > (1.0 / fps):
+        recorder.capture()
--- Data_Collection/keycapture.py
+++ Data_Collection/keycapture.py
@@ -1,6 +1,60 @@
 import os
 import pygetwindow as gw
 import keyboard
+from pynput import keyboard
+    
+
+def on_press(key):
+    print(key)
+    
+def on_release(key):
+    print(key)
+
+class KeyListener():
+    def __init__(self,folder_path,keys):
+        self.file_path = os.path.join(folder_path, "keys.txt")
+
+        # Create new CSV file)
+        open(self.file_path, 'w')
+
+        # Parse the keys of interest, up to 10
+        self.key_list = keys.split(',', 10)
+
+        # Used for tracking the keys pressed during the current frame
+        self.keys_pressed = [0] * len(self.key_list)
+            
+
+    #def on_press(self,key):
+    #    for element in self.keys_pressed:
+    #        if self.key_list[element] == key:
+    #            self.keys_pressed[element] = 1
+		
+    #def on_release(self,key):
+    #    for element in self.keys_pressed:
+    #        if self.key_list[element] == key:
+    #            self.keys_pressed[element] = 0
+        
+    def write_keys(self,frame_number):
+        # Generate a new string to write to the CSV
+        csv_string = str(frame_number) + ','
+        
+        # Iterate through the key list
+        for key in self.keys_pressed:
+            # Write the status of each key
+            csv_string += str(key) + ','
+        # Remove the unnecessary ',' from the end of the string
+        csv_string = csv_string[:-1] + '\n'
+
+        with open(self.file_path, 'a', newline='') as csvfile:
+            csvfile.write(csv_string)
+            csvfile.close()
+
+    def start_listen(self):
+        listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+        listener.start()
+        listener.join() # wait till listener will stop
+        # other stuff 
+
 
 class KeyCapture():
     #Default constructor
@@ -13,6 +67,7 @@ class KeyCapture():
 
         # Parse the keys of interest, up to 10
         self.key_list = keys.split(',', 10)
+
 
     # Write the keys of interest which are being read
     def write_keys(self, frame_number):
@@ -32,4 +87,3 @@ class KeyCapture():
         with open(self.file_path, 'a', newline='') as csvfile:
             csvfile.write(csv_string)
             csvfile.close()
-		
